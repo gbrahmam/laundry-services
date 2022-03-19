865968802d74ef7011d9735c27c13f4af768bb1e
Bharath-Boggarapu <96713555+Bharath-Boggarapu@users.noreply.github.com>
2022-03-19 13:24:50 +0530

    Add files via upload

---
+++ server/models/orders.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    productType:{type:String,enum:["Shirts", "T-Shirts","Trousers","Jeans", "Boxers","Joggers","Others"], default:0}
+    washType:{type:Image, }
+})
