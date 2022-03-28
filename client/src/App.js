8ae60b0060ad53d8843f4693b4798d662802759d
Brahmam <ceasar1729@gmail.com>
2022-03-29 01:41:52 +0530

    create order final commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, createContext} from "react";
+import Login from "./components/login";
+import Register from "./components/register";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import Pastorders from "./components/Pastorders";
+import Createorder from "./components/createorder";
+import Orderlist from "./components/orderlist";
 
+export const store = createContext()
 function App() {
+  const[token, setToken] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App"></div>
+      <store.Provider value={[token, setToken]}>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+         <Route exact path="/register">
+            <Register />
+           </Route>
+          <Route exact path="/createorder">
+            <Createorder />
+          </Route>
+          <Route exact path="/orderlist">
+            <Orderlist />
+          </Route>
+          <Route path="/pastorders">
+            <Pastorders />
+          </Route> 
+        </Switch>
+      </store.Provider>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(
   <React.StrictMode>
